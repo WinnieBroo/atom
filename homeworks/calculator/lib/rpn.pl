cd9c9891df9a7284001ae2b22c0b4d6dfc65ea80
Maxim Tsyganov <winniebrooo@gmail.com>
2016-11-16 01:10:28 -0800

    Added rpn.pl

--- homeworks/calculator/lib/rpn.pl
+++ homeworks/calculator/lib/rpn.pl
@@ -10,7 +10,7 @@
 use 5.010;
 use strict;
 use warnings;
-use diagnostics;
+#use diagnostics;
 BEGIN{
 	if ($] < 5.018) {
 		package experimental;
@@ -21,14 +21,68 @@ no warnings 'experimental';
 use FindBin;
 require "$FindBin::Bin/../lib/tokenize.pl";
 
+sub priority {
+	my $operation = shift;
+	given ($operation) {
+		when (['U+','U-']) {return 4;}
+		when (['^']) {return 3;}
+		when (['*','/']) {return 2;}
+		when (['+','-']) {return 1;}
+		default {return 0;}
+	}
+}
+
 sub rpn {
 	my $expr = shift;
 	my $source = tokenize($expr);
 	my @rpn;
+	my @operation;
 
-	# ...
+	for my $foo (@{$source}) {
+		
+		given ($foo) {
+			
+			when (['+','-','*','/','U+','U-','^']) {
+				if ($foo =~ /U[\+\-]|\^/) {
+					while (priority($foo) < priority($operation[-1])) {
+						push(@rpn, pop(@operation))
+					}
+				} else {
+					while (priority($foo) <= priority($operation[-1])) {
+						push(@rpn, pop(@operation))
+					}
+				}
+				push(@operation, $foo)
+			}
+			
+			when ('(') {
+				push(@operation, $foo)
+			}
+
+			when (')') {
+				while ($operation[-1] ne '(') {
+					if (@operation) {push(@rpn, pop(@operation))}
+					else {die "Incorrect brackets"}
+				}
+				pop(@operation)
+			}
+
+			default {
+				$foo = 0 + $foo;
+				push(@rpn, pop(@operation))
+				}
+			}
+
+		}
+
+	while (@operation and $operation[-1] ne '(') {		
+		push (@rpn, pop(@operation));
+	};
+
+	if (@operation) {die "Wrong expression"};
 
 	return \@rpn;
-}
+
+};
 
 1;
